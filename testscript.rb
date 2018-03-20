4878d4f40ef098729de6c5c596e6b65a74ec4701
Dipen <dipen.ec2010@gmail.com>
2018-03-20 20:04:21 +0800

    script

---
+++ testscript.rb
@@ -0,0 +1,13 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/dipenpatel235/testscript/archive/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "6dc72e65c8fdd32821bcc6cf832cb971f0ee812d58bb7605c55e89ea6c0ed0e2"
+
+
+  def install
+	bin.install "testscript"
+  end
+
+end
